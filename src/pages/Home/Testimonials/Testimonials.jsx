b4463013e2b08615ad2e88a6227acf672afd43f2
kawsaruhb <kawsaruhb@gmail.com>
2023-06-20 21:32:40 +0600

    ratings added

---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
+import { FaQuoteLeft } from 'react-icons/fa';
+
+const Testimonials = () => {
+
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch('reviews.json')
+        .then(res => res.json())
+        .then(data => setReviews(data))
+    },[])
+
+  return (
+    <section className='mb-12'>
+      <SectionTitle
+        subHeading={'---What Our Clients Say---'}
+        heading={'TESTIMONIALS'}
+      ></SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]} className='mySwiper'>
+        
+        {
+            reviews.map(review => <SwiperSlide key={review._id}>
+                <div className='flex flex-col items-center mx-24'>
+                    <Rating
+                        style={{ maxWidth: 150 }}
+                        value={review.rating}
+                        readOnly
+                    />
+                    <FaQuoteLeft className='w-16 h-16 mt-10 mb-4 text-black'/>
+                    <p>{review.details}</p>
+                    <h3 className='text-2xl text-yellow-500'>{review.name}</h3>
+                </div>
+            </SwiperSlide>)
+        }
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
--- src/pages/PopularMenu/PopularMenu.jsx
+++ src/pages/PopularMenu/PopularMenu.jsx
@@ -19,13 +19,16 @@ const PopularMenu = () => {
         subHeading={'---Popular Items---'}
         heading={'FROM OUR MENU'}
       ></SectionTitle>
-      <div className='grid md:grid-cols-2 gap-10 mb-12'>
+      <div className='grid md:grid-cols-2 gap-10 mb-8'>
         {
             menu.map(item => <MenuItem
             key={item._id}
             item={item}
             ></MenuItem>)
         }
+      </div>
+      <div className='text-center'>
+            <button className="btn btn-outline border-0 border-b-4 mt-4 mb-14">View Full Menu</button>
       </div>
     </section>
   );
